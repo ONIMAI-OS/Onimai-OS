a7f7b2a6d6ba37b9d01702e3a3d628d4f01c41c2
spawnrys <spawnrys@noreply.localhost>
2023-11-20 11:45:39 +0300

    Updated flatpaks list

--- Gnome/install_flatpaks.sh
+++ Gnome/install_flatpaks.sh
@@ -5,31 +5,16 @@
 
 flatpak install flathub info.febvre.Komikku
 flatpak install flathub com.github.tchx84.Flatseal
-flatpak install flathub de.haeckerfelix.Shortwave
 flatpak install flathub org.gnome.Lollypop
-flatpak install flathub de.haeckerfelix.Fragments
 flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 flatpak install flathub com.mattjakeman.ExtensionManager
 
 cd flatpaks
-rm -rf nyarchtour.flatpak
-rm -rf catgirldownloader.flatpak
 rm -rf nyarchcustomize.flatpak
-rm -rf nyarchtour.flatpak
-rm -rf wizard.flatpak
 rm -rf nyarchscript.flatpak
-rm -rf waifudownloader.flatpak
 
-wget https://github.com/nyarchlinux/nyarchtour/releases/latest/download/nyarchtour.flatpak
-wget https://github.com/nyarchlinux/nyarchwizard/releases/latest/download/wizard.flatpak
-wget https://github.com/nyarchlinux/catgirldownloader/releases/latest/download/catgirldownloader.flatpak
 wget https://github.com/nyarchlinux/nyarchscript/releases/latest/download/nyarchscript.flatpak
 wget https://github.com/nyarchlinux/nyarchcustomize/releases/latest/download/nyarchcustomize.flatpak
-wget https://github.com/nyarchlinux/waifudownloader/releases/latest/download/waifudownloader.flatpak
 
-flatpak install nyarchtour.flatpak
-flatpak install catgirldownloader.flatpak
 flatpak install nyarchcustomize.flatpak
-flatpak install wizard.flatpak
 flatpak install nyarchscript.flatpak
-flatpak install waifudownloader.flatpak
