4095c1a2ca153dc1dbe34350b2c1897c728b1bd3
SPAWNRYS <spawnrys@internet.ru>
2023-11-24 08:01:26 +0300

    Edited color scheme in dmenu

--- Gnome/etc/skel/.cache/wal/colors-wal-dmenu.h
+++ Gnome/etc/skel/.cache/wal/colors-wal-dmenu.h
@@ -2,5 +2,5 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#f2e0cc", "#1f1b16" },
 	[SchemeSel] = { "#f2e0cc", "#CB7359" },
-	[SchemeOut] = { "#f2e0cc", "#E7AF91" },
+	[SchemeOut] = { "#f2e0cc", "#91c9e7" },
 };
